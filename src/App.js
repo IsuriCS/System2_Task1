e97eae75b859e2f1f70c09d6abe315d63c384f20
Isuri Chathurika <isurics2@gmail.com>
2024-11-13 11:24:13 +0530

    Resolve Comments

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
-// import './App.css';
 import Login from './Pages/login';
 import Welcome from './Pages/welcome';
 
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />}></Route>
         <Route path='/welcome' element={<Welcome />}></Route>
       </Routes>
-    </BrowserRouter>
-    // <div className="App">
-    //   <header className="App-header">
-    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    </BrowserRouter>   
   );
 }
 
--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,4 +1,3 @@
-import { event } from "jquery";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -7,105 +6,108 @@ export default () => {
   const [userName, setUserName] = useState(null);
   const [password, setPassword] = useState(null);
 
-  const [isUsernameActivated, setUsernameActivated] = useState(false);
+  const [isUsernameActivated, setUserNameActivated] = useState(false);
   const [isPasswordActivated, setPasswordActivated] = useState(false);
 
-  const [unalert, setUnAlert] = useState(null);
-  const [pwalert, setPwAlert] = useState(null);
-
-  function handleUsername(event){
-    setUserName(event.target.value);
-    
-    checkUsername(userName);
-  
-  }
-
-  function handleUnBlur() {
-    setUsernameActivated(true)
-    checkUsername(userName)
-  }
-    function handlePwBlur() {
-      setPasswordActivated(true);
-      checkPassword(password)
-    }
+  const [userNameAlert, setUserNameAlert] = useState(null);
+  const [passwordAlert, setPasswordAlert] = useState(null);
 
   function checkUsername(userName) {
+
     if (userName == null) {
-      setUnAlert("Please enter user name.")
+      setUserNameAlert("Please enter user name.");
     }
     else {
-      setUnAlert(null)
+      setUserNameAlert(null);
     }
   }
 
   function checkPassword(password) {
+
     if (password == null) {
-      setPwAlert("Please enter password.")
+      setPasswordAlert("Please enter password.");
     }
     else if (password.length < 5) {
-      setPwAlert("Password should be longer than 5 charactors.")
+      setPasswordAlert("Password should be longer than 5 charactors.");
     }
     else {
-      setPwAlert(null)
+      setPasswordAlert(null);
     }
   }
 
   function handleClick() {
+
     if (pwalert == null && unalert == null) {
       localStorage.setItem("UserName", userName);
       navigate("/welcome");
     }
   }
 
-  
-    return (
-      
-      <div className="border flex justify-center align-center items-center bg-bgColor h-screen">
-        <div className="flex flex-col space-y-8 bg-white p-10   border-red w-[30rem] items-center rounded-lg drop-shadow ">
-          <h1 className="text-4xl font-bold text-darkRed font-sans">Log In</h1>
-          <div className="flex flex-col  space-y-4 w-full">
-            <div className="w-full">
-              <input
-                type="text"
-                placeholder="User Name"
-                value={userName}
-                onBlur={handleUnBlur}
-                onChange={(e) => handleUsername(e)}
-                className="border border-Gray p-1 rounded-md drop-shadow w-full"
-              />
-              {isUsernameActivated && (
-                <div>
-                  <p className="text-red text-xs">{unalert}</p>
-                </div>
-              )}
-            </div>
-            <div className="w-full">
-              <input
-                type="password"
-                placeholder="Password"
-                onBlur={handlePwBlur}
-                className="border p-1 rounded-md border-Gray drop-shadow w-full"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {isPasswordActivated && (
-                <div>
-                  <p className="text-red text-xs">{pwalert}</p>
-                </div>
-              )}
-            </div>
+  return (
+    <div className="border flex justify-center align-center items-center bg-bgColor h-screen">
+      <div className="flex flex-col space-y-8 bg-white p-10   border-red w-[30rem] items-center rounded-lg drop-shadow ">
+        <h1 className="text-4xl font-bold text-darkRed font-sans">Log In</h1>
+        <div className="flex flex-col  space-y-4 w-full">
+
+          <div className="w-full">
+            <input
+              type="text"
+              placeholder="User Name"
+              value={userName}
+              onBlur={() => {
+
+                setUserNameActivated(true);
+
+                checkUsername(userName);
+
+              }}
+              onChange={(e) => setUserName(e.target.value)}
+              className="border border-Gray p-1 rounded-md drop-shadow w-full"
+            />
+
+            {isUsernameActivated && (
+              <div>
+                <p className="text-red text-xs">{userNameAlert}</p>
+              </div>
+            )}
+
           </div>
-          <div>
-            {/* <button className="bg-red border p-1 rounded-full text-white text-xl py-2 px-4">Log In</button> */}
-            <button className="px-7 py-4 text-white  bg-button rounded-full hover:bg-buttonHover active:bg-buttonHover" onClick={handleClick}>
-              Log In
-            </button>
+
+          <div className="w-full">
+            <input
+              type="password"
+              placeholder="Password"
+              onBlur={() => {
+
+                 setPasswordActivated(true);
+
+                checkPassword(password);
+                
+              }}
+              className="border p-1 rounded-md border-Gray drop-shadow w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {isPasswordActivated && (
+              <div>
+                <p className="text-red text-xs">{passwordAlert}</p>
+              </div>
+            )}
+
           </div>
+          
+        </div>
+        <div>   
+          <button
+            className="px-7 py-4 text-white  bg-button rounded-full hover:bg-buttonHover active:bg-buttonHover"
+            onClick={handleClick}
+          >
+            Log In
+          </button>
         </div>
       </div>
-      // </div>
-    );
-    
-    
-    
+    </div>
+
+  );
 };
