45f1c355b567c9e24c19469bda47a23b6c356405
Isuri Chathurika <isurics2@gmail.com>
2024-11-13 16:38:27 +0530

    add text area

--- src/Pages/welcome.jsx
+++ src/Pages/welcome.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import Wave from "../Assets/koala-hug.gif"
+import {TextArea} from "../Components/Input";
 
 export default () => {
-  const [name, setName] = useState(localStorage.getItem("UserName") || "There");
+
+  const [typeText, setTypeText] = useState('');
+
     return (
-        <div className="w-screen h-screen bg-bgColor flex items-center justify-center flex-col">
-            <h1 className="text-6xl">Hello {name}</h1>
-            <img src={Wave} alt="" />
-        </div>
-  );
+      <div className="w-screen h-screen bg-bgColor flex items-center justify-center flex-col">
+        <h1 className="text-6xl">Hi</h1>
+        <p className="text-4xl ">{typeText}</p>
+        <img src={Wave} alt="" />
+
+        <TextArea
+          placeHolder="Type something here"
+          value={typeText}
+          Onchange={(e) => setTypeText(e.target.value)}
+        />
+        
+      </div>
+    );
 };
