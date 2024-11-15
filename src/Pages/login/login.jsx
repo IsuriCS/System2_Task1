ca153e42d369bc205aadb25ac8be74ce4f8df801
Isuri Chathurika <isurics2@gmail.com>
2024-11-15 21:29:36 +0530

    Correct routing imports in app.js

--- src/Pages/login/login.jsx
+++ src/Pages/login/login.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {InputFiled} from "../Components/Input";
-
 
 export default () => {
   const navigate = useNavigate();
@@ -14,34 +12,28 @@ export default () => {
   const [userNameAlert, setUserNameAlert] = useState(null);
   const [passwordAlert, setPasswordAlert] = useState(null);
 
-  const [isInputsValid, setInputValid] = useState(null);
-
-  function checkUsername(userName) {
+  function checkUsername () {
     if (userName == null) {
-      setUserNameAlert("User Name Is Required");
-    }
-    else {
+      setUserNameAlert("Please enter user name.");
+    } else {
       setUserNameAlert(null);
     }
-  }
+  };
 
   function checkPassword(password) {
     if (password == null) {
-      setPasswordAlert("Password is Required.");
-    }
-    else {
+      setPasswordAlert("Please enter password.");
+    } else if (password.length < 5) {
+      setPasswordAlert("Password should be longer than 5 charactors.");
+    } else {
       setPasswordAlert(null);
     }
   }
 
   function handleClick() {
-
-    if (userName==='testUser' && password==='abc123') {
-      setInputValid(true)
+    if (passwordAlert == null && userNameAlert == null) {
+      localStorage.setItem("UserName", userName);
       navigate("/welcome");
-    }
-    else {
-      setInputValid(false)
     }
   }
 
@@ -50,18 +42,20 @@ export default () => {
       <div className="flex flex-col space-y-8 bg-white p-10   border-red w-[30rem] items-center rounded-lg drop-shadow ">
         <h1 className="text-4xl font-bold text-darkRed font-sans">Log In</h1>
         <div className="flex flex-col  space-y-4 w-full">
-          
           <div className="w-full">
-            <InputFiled
+            <input
               type="text"
-              placeHolder="User Name"
+              placeholder="User Name"
               value={userName}
               onBlur={() => {
                 setUserNameActivated(true);
+
                 checkUsername(userName);
               }}
-              Onchange={(e) => setUserName(e.target.value)}
+              onChange={(e) => setUserName(e.target.value)}
+              className="border border-Gray p-1 rounded-md drop-shadow w-full"
             />
+
             {isUsernameActivated && (
               <div>
                 <p className="text-red text-xs">{userNameAlert}</p>
@@ -70,16 +64,19 @@ export default () => {
           </div>
 
           <div className="w-full">
-            <InputFiled
+            <input
               type="password"
-              placeHolder="Password"
-              value={password}
+              placeholder="Password"
               onBlur={() => {
                 setPasswordActivated(true);
+
                 checkPassword(password);
               }}
-              Onchange={(e) => setPassword(e.target.value)}
+              className="border p-1 rounded-md border-Gray drop-shadow w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
+
             {isPasswordActivated && (
               <div>
                 <p className="text-red text-xs">{passwordAlert}</p>
@@ -87,13 +84,6 @@ export default () => {
             )}
           </div>
         </div>
-
-        {isInputsValid === false && (
-          <div>
-            <p className="text-red text-xs">Invalid Username or Password</p>
-          </div>
-        )}
-
         <div>
           <button
             className="px-7 py-4 text-white  bg-button rounded-full hover:bg-buttonHover active:bg-buttonHover"
